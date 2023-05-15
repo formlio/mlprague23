da3113148481b856f80418e5dac2dc3b620463f0
antonymayi <antonymayi@yahoo.com>
2023-05-15 22:17:16 +0000

    save

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,35 @@
+"""
+This is a FooBar schema catalog used throughout the ForML tutorial.
+"""
 from forml.io import dsl
 
 
-class Dummy(dsl.Schema):
-    """Dummy dataset schema."""
+class Foo(dsl.Schema):
+    """Foo dataset schema."""
 
-    Title = dsl.Field(dsl.String())
-    Key = dsl.Field(dsl.Integer())
-    Target = dsl.Field(dsl.Float())
     Timestamp = dsl.Field(dsl.Timestamp())
+    Label = dsl.Field(dsl.Integer())
+    Code = dsl.Field(dsl.String())
+    Value = dsl.Field(dsl.Float())
+    Bar = dsl.Field(dsl.Integer())
+
+
+class Bar(dsl.Schema):
+    """Bar dataset schema."""
+
+    Key = dsl.Field(dsl.Integer())
+    Length = dsl.Field(dsl.Float())
+    Color = dsl.Field(dsl.String())
+
+
+class Baz(dsl.Schema):
+    """Baz dataset schema.
+
+    Note: To demonstrate an unavailable dataset, this schema is deliberately
+          not resolved by any of the feeds configured in this tutorial
+          environment.
+    """
+
+    Key = dsl.Field(dsl.Integer())
+    Name = dsl.Field(dsl.String())
+    Dob = dsl.Field(dsl.Date())
